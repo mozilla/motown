6c3e86b590dc6f0a18bcf3057f2dedf238485ce2
Simon Wex <simon@simonwex.com>
2012-06-14 17:19:56 -0700

    Keeping the session cookie around for a year.

--- app/http/server.js
+++ app/http/server.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
+require('../../lib/extensions/number');
 
 // Module dependencies.
 const 
@@ -92,7 +92,8 @@ http.configure(function(){
   http.use(express.session({
     secret: 'As a kid I ran down the stairs at full speed because I imagined Indians with bows and arrows would hit me if I walked.',
     key: 'express.sid',
-    store: sessionStore
+    store: sessionStore,
+    cookie: {maxAge: (365).days()}
   }));
 
   http.use(function (req, res, next) {
